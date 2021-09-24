fe5032d851b5a86d57e97253cc3c96b2ae504e0c
anvar_thinkland <anvar@thinkland.uz>
2021-09-24 15:25:05 +0500

    admin page logic written for VIN model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from api import models
+
+
+@admin.register(models.VIN)
+class VINAdmin(admin.ModelAdmin):
+    list_display = [
+        'vin_code',
+        'make',
+        'model',
+        'year',
+        'manufacturer',
+        'engine',
+        'trim',
+        'transmission'
+    ]
+    search_fields = [
+        'vin_code',
+        'make',
+        'model',
+        'manufacturer'
+    ]
+    list_filter = [
+        'make',
+        'manufacturer',
+        'year'
+    ]
